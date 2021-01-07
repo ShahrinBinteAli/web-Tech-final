8ad310a586c371f62c8a1206c649df1db6705ac3
ShahrinBinteAli <60428392+ShahrinBinteAli@users.noreply.github.com>
2021-01-07 23:48:50 +0600

    d

---
+++ course.php
@@ -0,0 +1,215 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form >
+<table border="1">
+	<tr>
+		<td colspan="4"><center><h3>Offered Courses</h3></center></td>
+	</tr>
+	<tr>
+		
+		<td>
+			COURSE
+		</td>
+		<td>
+			SECTION
+		</td>
+		<td>
+			CAPACITY
+		</td>
+		<td>
+			UPDATE
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			Web Technology
+		</td>
+		<td>
+			A mon(8.00a.m-11.00a.m),wed(10.00a.m-12.00a.m)<input type="submit" name="select" value="select"></br>
+			B wed(8.00a.m-10.00a.m),mon(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			C mon(10.00a.m-12.00a.m),wed(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			D wed(12.00a.m-2.00p.m),mon(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			E mon(2.00p.m-4.00p.m),wed(2.00p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			40</br>
+			40</br>
+			40</br>
+			40</br>
+			40
+		</td>
+		<td>
+			 39/40</br>
+			 38/40</br>
+			 39/40</br>
+			 39/40</br>
+			 40/40
+		</td>
+	</tr>
+   </tr>
+	<tr>
+		
+		<td>
+			Artificial Intelligence
+		</td>
+		<td>
+			A mon(8.00a.m-11.00a.m),wed(10.00a.m-12.00a.m)<input type="submit" name="select" value="select"></br>
+			B wed(8.00a.m-10.00a.m),mon(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			C mon(10.00a.m-12.00a.m),wed(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			D wed(12.00a.m-2.00p.m),mon(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			E mon(2.00p.m-4.00p.m),wed(2.00p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			40</br>
+			40</br>
+			40</br>
+			40</br>
+			40
+		</td>
+		<td>
+			39/40</br>
+			 38/40</br>
+			 40/40</br>
+			 38/40</br>
+			 40/40 
+		</td>
+	</tr>
+	</tr>
+	<tr>
+		
+		<td>
+			Software Engineering
+		</td>
+		<td>
+			A sun,tue(11.00a.m-12.30a.m)<input type="submit" name="select" value="select"></br>
+			B tue,sun(2.00p.m-3.30p.m)<input type="submit" name="select" value="select"></br>
+			C sun,tue(8.00A.m-9.30A.m)<input type="submit" name="select" value="select"></br>
+			D mon,wed(2.00p.m-3.30p.m)<input type="submit" name="select" value="select"></br>
+			E sun,tue(9.30a.m-11.00.m)<input type="submit" name="select" value="select"></br>
+			F sun,tue(12.30a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			G mon,wed(3.30p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30
+			
+		</td>
+		<td>
+			30/30</br>
+			30/30</br>
+			30/29</br>
+			30/25</br>
+			30/30</br>
+			30/30</br>
+			30/29
+		</td>
+	</tr>
+	<tr>
+		
+		<td>
+			Advanced Topic in Programming 3
+		</td>
+		<td>
+			A mon(10.00a.m-12.00a.m),wed(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			B wed(12.00a.m-2.00p.m),mon(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			C mon(2.00p.m-4.00p.m),wed(2.00p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			40</br>
+			40</br>
+			40
+		</td>
+		<td>
+			40/40</br>
+			40/40</br>
+			40/40 
+		</td>
+	</tr>
+	<tr>
+		
+		<td>
+			Object Oriented Programming 2
+		</td>
+		<td>
+			A sun(8.00a.m-10.00a.m),tue(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			B tue(10.00a.m-12.00a.m),sun(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			C tue(12.00a.m-2.00p.m),sun(11.00a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			D mon(2.00p.m-4.00p.m),wed(2.00p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			40</br>
+			40</br>
+			40</br>
+			40
+		</td>
+		<td>
+			40/40</br>
+			40/40</br>
+			40/40</br>
+			39/40
+		</td>
+	</tr>
+	<tr>
+		
+		<td>
+			Compiler Design
+		</td>
+		<td>
+			A sun,tue(11.00a.m-12.30a.m)<input type="submit" name="select" value="select"></br>
+			B tue,sun(2.00p.m-3.30p.m)<input type="submit" name="select" value="select"></br>
+			C sun,tue(8.00A.m-9.30A.m)<input type="submit" name="select" value="select"></br>
+			D mon,wed(2.00p.m-3.30p.m)<input type="submit" name="select" value="select"></br>
+			E sun,tue(9.30a.m-11.00.m)<input type="submit" name="select" value="select"></br>
+			F sun,tue(12.30a.m-2.00p.m)<input type="submit" name="select" value="select"></br>
+			G mon,wed(3.30p.m-5.00p.m)<input type="submit" name="select" value="select"></br>
+		</td>
+		<td>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30</br>
+			30
+		</td>
+		<td>
+			30/30</br>
+			30/30</br>
+			30/29</br>
+			30/25</br>
+			30/30</br>
+			30/30</br>
+			30/29 
+		</td>
+	</tr>
+
+
+
+	<tr>
+		<td>
+	
+	<a href="home.php">go home </a>
+	
+	</td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
+	<?php session_destroy();
+	
+	header("location: registration.html");?>
