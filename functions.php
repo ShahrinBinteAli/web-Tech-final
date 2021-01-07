25d5e810b82421b2b9e3e9a7b095370c29d351f6
ShahrinBinteAli <60428392+ShahrinBinteAli@users.noreply.github.com>
2021-01-07 23:50:53 +0600

    f

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+	
+	require('db.php');
+	
+	function validate($username, $password){
+
+		$con = getConnection();
+		$sql = "select * from users  where username='{$username}' and password='{$password}'";
+		$result = mysqli_query($con, $sql);
+		$user = mysqli_fetch_assoc($result);
+
+		return $user;
+	}
+
+
+	function getAllUsers(){
+		$con = getConnection();
+		$sql = "select * from users";
+		$result = mysqli_query($con, $sql);
+		return $result;
+	}
+session_destroy();
+	
+	header("location: registration.html");
+?>
