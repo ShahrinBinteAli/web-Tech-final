e4a16d48cb6124dabeccff0bbc23ceee8d5cd60c
ShahrinBinteAli <60428392+ShahrinBinteAli@users.noreply.github.com>
2021-01-07 23:48:01 +0600

    c

---
+++ contact.php
@@ -0,0 +1,781 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student-Parent Contact info</title>
+</head>
+<body>
+
+<form >
+<table border="1">
+	<tr>
+		<td colspan="5"><center><h3>web tech (A)</h3></center></td>
+	</tr>
+	<tr>
+		
+		<td>
+			NAME
+		</td>
+		<td>
+			ID
+		</td>
+		<td>
+			CONTACT NO(STUDENT)
+		</td>
+		<td>
+			PARENT NAME
+		</td>
+		<td>
+			CONTACT NO(PARENT)
+		</td>
+		
+		
+	</tr>
+    <tr>
+		<td>
+			Adam
+		</td>
+		<td>
+			16-36597-1
+		</td>
+		<td>
+			1245678<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			XYZ
+		</td>
+		<td>
+			45678902<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			carry
+		</td>
+		<td>
+			17-36597-1
+		</td>
+		<td>
+			12345678<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			XYZ
+		</td>
+		<td>
+			4678916<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+		
+	</tr>
+	<tr>
+		<td>
+			sara
+		</td>
+		<td>
+			16-30597-3
+		</td>
+		<td>
+			6464644<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			XYZ
+		</td>
+		<td>
+			54646879<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			bob
+		</td>
+		<td>
+			17-36597-2
+		</td>
+		<td>
+			4646456<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			XYZ
+		</td>
+		<td>
+			48974854<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			marry
+		</td>
+		<td>
+			17-36588-1
+		</td>
+		<td>
+			546854<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YGY
+		</td>
+		<td>
+			546468<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			jon
+		</td>
+		<td>
+			16-30597-3
+		</td>
+		<td>
+			T546566<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UH
+		</td>
+		<td>
+			4645331<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			smith
+		</td>
+		<td>
+			18-36597-3
+		</td>
+		<td>
+			789746<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UGYU
+		</td>
+		<td>
+			5895465<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+	</table>
+
+<table border="1">
+	<tr>
+		<td colspan="5"><center><h3>Student info- Web Tech(B)</h3></center></td>
+	</tr>
+	
+	<tr>
+		
+		<td>
+			NAME
+		</td>
+		<td>
+			ID
+		</td>
+		<td>
+			CONTACT NO(STUDENT)
+		</td>
+		<td>
+			PARENT NAME
+		</td>
+		<td>
+			CONTACT NO(PARENT)
+		</td>
+		
+		
+	</tr>
+    <tr>
+		<td>
+			PAUL
+		</td>
+		<td>
+			16-36597-3
+		</td>
+		<td>
+			464654<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UGU
+		</td>
+		<td>
+			451326<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			JOE
+		</td>
+		<td>
+			17-36597-1
+		</td>
+		<td>
+			789454<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			GFH
+		</td>
+		<td>
+			87846<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			HENRY
+		</td>
+		<td>
+			17-36597-3
+		</td>
+		<td>
+			546454<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			HG
+		</td>
+		<td>
+			789484<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+	<tr>
+		<td>
+			ATISH
+		</td>
+		<td>
+			17-36597-1
+		</td>
+		<td>
+			57468746<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+		    UIO
+		</td>
+		<td>
+			54867464<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			ROSY
+		</td>
+		<td>
+			17-36588-1
+		</td>
+		<td>
+			797464<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YUK
+		</td>
+		<td>
+			546464<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			KELLY
+		</td>
+		<td>
+			16-30590-3
+		</td>
+		<td>
+			84864<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YTF
+		</td>
+		<td>
+			74846<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			EMMA
+		</td>
+		<td>
+			18-36547-1
+		</td>
+		<td>
+			97948<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			IJI
+		</td>
+		<td>
+			87889<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	
+</table>
+
+<table border="1">
+	<tr>
+		<td colspan="5"><center><h3>Student info- Artificial Intelligence(A)</h3></center></td>
+	</tr>
+	
+	<tr>
+		
+		<td>
+			NAME
+		</td>
+		<td>
+			ID
+		</td>
+		<td>
+			CONTACT NO(STUDENT)
+		</td>
+		<td>
+			PARENT NAME 
+		</td>
+		<td>
+			CONTACT NO(PARENT)
+		</td>
+		
+		
+	</tr>
+    <tr>
+		<td>
+			Adam
+		</td>
+		<td>
+			16-36597-1
+		</td>
+		<td>
+			546856<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UU
+		</td>
+		<td>
+			64758<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			carry
+		</td>
+		<td>
+			17-36597-1
+		</td>
+		<td>
+			87984<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			GUH
+		</td>
+		<td>
+			484651<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+		
+	</tr>
+	<tr>
+		<td>
+			sara
+		</td>
+		<td>
+			16-30597-3
+		</td>
+		<td>
+			487984<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			GRDSR
+		</td>
+		<td>
+			16545<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+	<tr>
+		<td>
+			bob
+		</td>
+		<td>
+			17-36597-2
+		</td>
+		<td>
+			14564<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UHW
+		</td>
+		<td> 
+		    464564<input type="submit" name="Contact" value="Contact"></br>	
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			marry
+		</td>
+		<td>
+			17-36588-1
+		</td>
+		<td>
+			548784<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			HUJ
+		</td>
+		<td>
+			57456415<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			jon
+		</td>
+		<td>
+			16-30597-3
+		</td>
+		<td>
+			4564<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YT
+		</td>
+		<td>
+			84784<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			smith
+		</td>
+		<td>
+			18-36597-3
+		</td>
+		<td>
+			4844<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			TFT
+		</td>
+		<td>
+			84845<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+	
+</table>
+
+<table border="1">
+	<tr>
+		<td colspan="5"><center><h3>Student info- Software Enginnering(A)</h3></center></td>
+	</tr>
+	
+	<tr>
+		
+		<td>
+			NAME
+		</td>
+		<td>
+			ID
+		</td>
+		<td>
+			CONTACT NO(STUDENT)
+		</td>
+		<td>
+			PARENT NAME
+		</td>
+		<td>
+			CONTACT NO(PARENT)
+		</td>
+		
+		
+	</tr>
+    <tr>
+		<td>
+			Asha
+		</td>
+		<td>
+			16-36597-1
+		</td>
+		<td>
+			48564<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			GYG
+		</td>
+		<td>
+			26021485<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			perry
+		</td>
+		<td>
+			17-36588-1
+		</td>
+		<td>
+			85478<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			SRE
+		</td>
+		<td>
+			1454787<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+		
+	</tr>
+	<tr>
+		<td>
+			ana
+		</td>
+		<td>
+			16-25597-1
+		</td>
+		<td>
+			878615<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YTY
+		</td>
+		<td>
+			4845<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+	<tr>
+		<td>
+			malcom
+		</td>
+		<td>
+			17-36555-2
+		</td>
+		<td>
+			79134<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			UHUI
+		</td>
+		<td>
+			74666<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			marry
+		</td>
+		<td>
+			17-36588-1
+		</td>
+		<td>
+			18751<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			TIJI
+		</td>
+		<td>
+			20791<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			jon
+		</td>
+		<td>
+			16-30597-3
+		</td>
+		<td>
+			879646<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YG
+		</td>
+		<td>
+			28756<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+</table>
+
+<table border="1">
+	<tr>
+		<td colspan="5"><center><h3>Student info- Software Enginnering(B)</h3></center></td>
+	</tr>
+	
+	<tr>
+		
+		<td>
+			NAME
+		</td>
+		<td>
+			ID
+		</td>
+		<td>
+			CONTACT NO(STUDENT)
+		</td>
+		<td>
+			PARENT NAME
+		</td>
+		<td>
+			CONTACT NO(PARENT)
+		</td>
+		
+		
+	</tr>
+    <tr>
+		<td>
+			PIYA
+		</td>
+		<td>
+			16-36597-3
+		</td>
+		<td>
+			19546<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YF
+		</td>
+		<td>
+			2982<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		
+		<td>
+			AMY
+		</td>
+		<td>
+			17-36597-3
+		</td>
+		<td>
+			896<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			OU
+		</td>
+		<td>
+			2097236<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+		
+	</tr>
+	<tr>
+		<td>
+			JACKSON
+		</td>
+		<td>
+			17-30597-3
+		</td>
+		<td>
+			79795<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			TYF
+		</td>
+		<td>
+			20978645<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			michel
+		</td>
+		<td>
+			17-36597-1
+		</td>
+		<td>
+			9978954<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			RD
+		</td>
+		<td>
+			25876<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			ally
+		</td>
+		<td>
+			18-36500-1
+		</td>
+		<td>
+			98794<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YTG
+		</td>
+		<td>
+			2074964<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+		
+	</tr>
+	<tr>
+		<td>
+			john
+		</td>
+		<td>
+			18-30597-2
+		</td>
+		<td>
+			10645<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		<td>
+			YF
+		</td>
+		<td>
+		    17984<input type="submit" name="Contact" value="Contact"></br>
+		</td>
+		
+	</tr>
+</table>
+
+</form>
+<a href="home.php">go home </a>
+</body>
+</html>
+<?php session_destroy();
+	
+	header("location: registration.html");?>
